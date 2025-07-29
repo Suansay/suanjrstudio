e6a519fa71263bac9627d2a27d95017deb5cf29d
Suansay <150304166+Suansay@users.noreply.github.com>
2025-07-29 03:12:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css" type="text/css" media="all">
+  <!--<a href="https://icons8.com">Icon pack by Icons8</a>-->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald:300|Sacramento" rel="stylesheet">
+  <title> Studio </title>
+</head>
+
+<body>
+
+is.suanjr.studio.com
---
+++ app.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '8e115e';
+	} else {
+		header.style.backgroundColor = 'https://tse2.mm.bing.net/th?id=OIP.Qkw4zMNqTB_yi1t4K7jgigHaFL&pid=Api&P=0&h=180';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
